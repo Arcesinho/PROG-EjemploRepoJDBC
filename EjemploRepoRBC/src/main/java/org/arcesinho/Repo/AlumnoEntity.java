fd6040ca754dc5fbaf9979fd65e870f2953b32a9
a25adrianbt <adrian.balboa.1608@gmail.com>
2026-03-13 19:27:53 +0100

    Finalizado

--- EjemploRepoRBC/src/main/java/org/arcesinho/Repo/AlumnoEntity.java
+++ EjemploRepoRBC/src/main/java/org/arcesinho/Repo/AlumnoEntity.java
@@ -1,11 +1,23 @@
 package org.arcesinho.Repo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alumnos")
 public class AlumnoEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String apellidos;
     private String dni;
+    @Column(name="id_grupo")
     private int grupoid;
 
     public AlumnoEntity(int id, String nombre, String apellidos, String dni, int grupoid) {
@@ -23,6 +35,7 @@ public class AlumnoEntity {
     public void setId(int id) {
         this.id = id;
     }
+    
 
     public String getNombre() {
         return nombre;
